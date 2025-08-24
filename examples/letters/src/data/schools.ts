263bb5f16e1e286bce1be420d23eeaaeacf5bca1
JP Dillingham <jp@dillingham.me>
2025-08-23 19:41:11 -0500

    add a few schools

---
+++ examples/letters/src/data/schools.ts
@@ -0,0 +1,104 @@
+import { TuitionFitSchool } from '../types';
+
+export const schools: TuitionFitSchool[] = [
+  {
+    "ipedsId": "100654",
+    "federalSchoolCode": "001002",
+    "name": "Alabama A & M University",
+    "alias": "AAMU",
+    "city": "Normal",
+    "state": "AL",
+    "mainCampus": "1",
+    "tags": "NULL"
+  },
+  {
+    "ipedsId": "100663",
+    "federalSchoolCode": "001052",
+    "name": "University of Alabama at Birmingham",
+    "alias": "UAB",
+    "city": "Birmingham",
+    "state": "AL",
+    "mainCampus": "1",
+    "tags": "NULL"
+  },
+  {
+    "ipedsId": "100690",
+    "federalSchoolCode": "016885",
+    "name": "Amridge University",
+    "alias": "Southern Christian University  Regions University",
+    "city": "Montgomery",
+    "state": "AL",
+    "mainCampus": "1",
+    "tags": "NULL"
+  },
+  {
+    "ipedsId": "100706",
+    "federalSchoolCode": "001055",
+    "name": "University of Alabama in Huntsville",
+    "alias": "UAH  University of Alabama Huntsville",
+    "city": "Huntsville",
+    "state": "AL",
+    "mainCampus": "1",
+    "tags": "NULL"
+  },
+  {
+    "ipedsId": "100724",
+    "federalSchoolCode": "001005",
+    "name": "Alabama State University",
+    "alias": "NULL",
+    "city": "Montgomery",
+    "state": "AL",
+    "mainCampus": "1",
+    "tags": "NULL"
+  },
+  {
+    "ipedsId": "100751",
+    "federalSchoolCode": "001051",
+    "name": "The University of Alabama",
+    "alias": "NULL",
+    "city": "Tuscaloosa",
+    "state": "AL",
+    "mainCampus": "1",
+    "tags": "NULL"
+  },
+  {
+    "ipedsId": "100760",
+    "federalSchoolCode": "001007",
+    "name": "Central Alabama Community College",
+    "alias": "NULL",
+    "city": "Alexander City",
+    "state": "AL",
+    "mainCampus": "1",
+    "tags": "NULL"
+  },
+  {
+    "ipedsId": "100812",
+    "federalSchoolCode": "001008",
+    "name": "Athens State University",
+    "alias": "NULL",
+    "city": "Athens",
+    "state": "AL",
+    "mainCampus": "1",
+    "tags": "NULL"
+  },
+  {
+    "ipedsId": "100830",
+    "federalSchoolCode": "008310",
+    "name": "Auburn University at Montgomery",
+    "alias": "AUM||Auburn University at Montgomery|Auburn Montgomery",
+    "city": "Montgomery",
+    "state": "AL",
+    "mainCampus": "1",
+    "tags": "NULL"
+  },
+  {
+    "ipedsId": "100858",
+    "federalSchoolCode": "001009",
+    "name": "Auburn University",
+    "alias": "NULL",
+    "city": "Auburn",
+    "state": "AL",
+    "mainCampus": "1",
+    "tags": "NULL"
+  }
+];
